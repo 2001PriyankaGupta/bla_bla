76234f48ebdd9766424c712cfbe6f0f73e5a4fb6
2001PriyankaGupta <2001priyankagupta@gmail.com>
2026-02-12 23:23:57 +0530

    remove test

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/test', function () {
-    return "test";
-});
 
 Route::get('/', function () {
     if (Auth::check() && Auth::user()->is_admin == 1) {
